873e71f160149f9179232c0fa869e111123220d6
nayemalam110 <nayemalam110@gmail.com>
2021-08-30 09:17:02 +0600

    funtctionAdd

---
+++ index.js
@@ -0,0 +1,25 @@
+var btn = document.querySelector(".btn-1");
+
+
+  console.log(document.querySelector(".btn-1").value);
+
+    document.querySelector(".btn-1").addEventListener("click", function(){
+
+        var trx = document.querySelector(".btn-1").value;
+        document.querySelector("#input1").value = trx;
+
+    })
+
+
+    // function clc(){
+    //     var trx = document.querySelector(".btn-1").value;
+    //     document.querySelector("#input1").value = trx;
+    // }
+
+    // clc();
+
+
+
+
+    
+
